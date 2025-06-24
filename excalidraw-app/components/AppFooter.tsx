0e0e7acdcf72d38e1dc4d111cde95a6d4bc372d4
Pelle <52049523+farapholch@users.noreply.github.com>
2025-06-24 20:39:11 +0200

    Update AppFooter.tsx

--- excalidraw-app/components/AppFooter.tsx
+++ excalidraw-app/components/AppFooter.tsx
@@ -4,9 +4,10 @@ import { EncryptedIcon } from "./EncryptedIcon";
 import { ExcalidrawPlusAppLink } from "./ExcalidrawPlusAppLink";
 import { isExcalidrawPlusSignedUser } from "../app_constants";
 import { DebugFooter, isVisualDebuggerEnabled } from "./DebugCanvas";
+import RoomInfo from "./RoomEndTime";
 
 export const AppFooter = React.memo(
-  ({ onChange }: { onChange: () => void }) => {
+  ({ onChange, portal }: { onChange: () => void; portal: { roomId?: string } }) => {
     return (
       <Footer>
         <div
@@ -16,12 +17,8 @@ export const AppFooter = React.memo(
             alignItems: "center",
           }}
         >
-          {isVisualDebuggerEnabled() && <DebugFooter onChange={onChange} />}
-          {isExcalidrawPlusSignedUser ? (
-            <ExcalidrawPlusAppLink />
-          ) : (
-            <EncryptedIcon />
-          )}
+          <EncryptedIcon />
+          <RoomInfo portal={portal} />
         </div>
       </Footer>
     );
